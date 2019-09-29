1c60ba4420321dbd38eb0c8efb2973f63171e31c
Luis Sanchez <sase.priv@gmail.com>
2019-09-29 13:30:35 -0400

    Agregando la parte visual

--- Inicio.Designer.cs
+++ Inicio.Designer.cs
@@ -28,21 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ch_name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_pid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_description = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_resume = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel2.Controls.Add(this.btn_start);
+            this.flowLayoutPanel2.Controls.Add(this.btn_stop);
+            this.flowLayoutPanel2.Controls.Add(this.btn_pause);
+            this.flowLayoutPanel2.Controls.Add(this.btn_resume);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(66, 623);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(386, 44);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ch_name,
+            this.ch_pid,
+            this.ch_status,
+            this.ch_description});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(520, 596);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ch_name
+            // 
+            this.ch_name.Text = "ServiceName";
+            this.ch_name.Width = 121;
+            // 
+            // ch_pid
+            // 
+            this.ch_pid.Text = "PID";
+            // 
+            // ch_description
+            // 
+            this.ch_description.Text = "Descripcion";
+            this.ch_description.Width = 231;
+            // 
+            // ch_status
+            // 
+            this.ch_status.Text = "Status";
+            this.ch_status.Width = 104;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(3, 3);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(90, 27);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.Location = new System.Drawing.Point(195, 3);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(90, 27);
+            this.btn_pause.TabIndex = 1;
+            this.btn_pause.Text = "Pause";
+            this.btn_pause.UseVisualStyleBackColor = true;
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(99, 3);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(90, 27);
+            this.btn_stop.TabIndex = 2;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            // 
+            // btn_resume
+            // 
+            this.btn_resume.Location = new System.Drawing.Point(291, 3);
+            this.btn_resume.Name = "btn_resume";
+            this.btn_resume.Size = new System.Drawing.Size(90, 27);
+            this.btn_resume.TabIndex = 3;
+            this.btn_resume.Text = "Resume";
+            this.btn_resume.UseVisualStyleBackColor = true;
             // 
             // Inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1303, 723);
+            this.ClientSize = new System.Drawing.Size(544, 669);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.flowLayoutPanel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Inicio";
-            this.Text = "         ";
+            this.Text = "Servicios";
+            this.flowLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_resume;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ch_name;
+        private System.Windows.Forms.ColumnHeader ch_pid;
+        private System.Windows.Forms.ColumnHeader ch_status;
+        private System.Windows.Forms.ColumnHeader ch_description;
     }
 }
 
