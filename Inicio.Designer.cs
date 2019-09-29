ff9aa52d28d477195f27bd4246f48fa0e25c5df0
Luis Sanchez <sase.priv@gmail.com>
2019-09-29 11:31:22 -0400

    mi primer commit

--- Inicio.Designer.cs
+++ Inicio.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VisualizacionDeProcesos
 {
-    partial class Form1
+    partial class Inicio
     {
         /// <summary>
         /// Required designer variable.
@@ -30,12 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // Inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1303, 723);
-            this.Name = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Inicio";
             this.Text = "         ";
             this.ResumeLayout(false);
 
